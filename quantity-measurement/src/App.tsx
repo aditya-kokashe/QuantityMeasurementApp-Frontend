891b82c4938860a234c9dce4e0c147566dc957ba
aditya-kokashe <adi310kokashe@gmail.com>
2026-04-01 23:43:25 +0530

    [Aditya Kokashe] : added App.tsx program defining root component structure

---
+++ quantity-measurement/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import DashboardPage from "./pages/DashboardPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
